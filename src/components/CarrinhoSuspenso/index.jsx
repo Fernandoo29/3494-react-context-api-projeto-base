9c15c82abdaba05ca1d01d994309bdbd14481e0a
Fernando <fernandobernardoo29@gmail.com>
2025-02-04 09:19:37 -0300

    comitts até a aula 4

--- src/components/CarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
+
 import Botao from "@/components/Botao";
 import ListaProdutosCarrinho from "@/components/ListaProdutosCarrinho";
 import TotalCarrinho from "./TotalCarrinho";
 import Titulo from "@/components/Titulo";
 
-const CarrinhoSuspenso = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-  valorTotalCarrinho,
-}) => {
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
+
+const CarrinhoSuspenso = () => {
+  const { carrinho, valorTotal } = useCarrinhoContext()
   return (
     <div
       className="offcanvas offcanvas-end text-bg-dark"
@@ -34,13 +32,8 @@ const CarrinhoSuspenso = ({
         ></Botao>
       </div>
       <div className="offcanvas-body">
-        <ListaProdutosCarrinho
-          carrinho={carrinho}
-          adicionarProduto={adicionarProduto}
-          removerProduto={removerProduto}
-          removerProdutoCarrinho={removerProdutoCarrinho}
-        />
-        <TotalCarrinho valorTotalCarrinho={valorTotalCarrinho} />
+        <ListaProdutosCarrinho carrinho={carrinho} />
+        <TotalCarrinho valorTotalCarrinho={valorTotal} />
       </div>
     </div>
   );
